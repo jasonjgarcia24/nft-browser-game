2be8474e56a32acc0db07cd4765f5e08425e17a2
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-10-27 18:32:47 -0700

    Added keys to .env.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,15 @@
 require('@nomiclabs/hardhat-waffle');
+require("dotenv").config();
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL
+const PRIVATE_RINKEBY_ACCOUNT_KEY = process.env.PRIVATE_RINKEBY_ACCOUNT_KEY
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'YOUR ALCHEMY_API_UR',
-      accounts: ['YOUR_PRIVATE_RINKEBY_ACCOUNT_KEY'],
+      url: ALCHEMY_API_URL,
+      accounts: [PRIVATE_RINKEBY_ACCOUNT_KEY],
     },
   },
 };
